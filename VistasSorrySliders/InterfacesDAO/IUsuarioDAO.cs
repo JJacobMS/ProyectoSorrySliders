aee7c1d05100c700e86c409a1eb654d748f8b16e
melus7n7 <sulem477@gmail.com>
2023-09-29 08:28:57 -0600

    Implementación inicial del inicio de sesión

---
+++ VistasSorrySliders/InterfacesDAO/IUsuarioDAO.cs
@@ -0,0 +1,14 @@
+﻿using BibliotecaClasesSorrySliders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VistasSorrySliders.InterfacesDAO
+{
+    public interface IUsuarioDAO
+    {
+        
+    }
+}
