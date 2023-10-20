8e3996444de771d4fd681c95014497a6ed5feb31
melus7n7 <sulem477@gmail.com>
2023-10-20 14:07:15 -0600

    Detalles Usuario

--- VistasSorrySliders/CuentaDetallesVentana.xaml.cs
+++ VistasSorrySliders/CuentaDetallesVentana.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -21,17 +22,68 @@ namespace VistasSorrySliders
     public partial class CuentaDetallesVentana : Window
     {
         private CuentaSet _cuenta;
+        private UsuarioSet _usuario;
         public CuentaDetallesVentana(CuentaSet cuenta)
         {
             InitializeComponent();
             _cuenta = cuenta;
+            RecuperarDatos();
             ColocarDatos();
+
         }
 
         private void ColocarDatos()
         {
+            txtBoxNombre.Text = _usuario.Nombre;
+            txtBoxApellidos.Text = _usuario.Apellido;
             txtBoxCorreo.Text = _cuenta.CorreoElectronico;
             txtBoxNickname.Text = _cuenta.Nickname;
+            llpAvatar.Fill = Utilidades.ConvertirBytesAImageBrush(_cuenta.Avatar);
+        }
+        
+        private void RecuperarDatos()
+        {
+            Constantes resultado;
+            try
+            {
+                DetallesCuentaUsuarioClient proxyUsuario = new DetallesCuentaUsuarioClient();
+                (resultado, _usuario) = proxyUsuario.RecuperarDatosUsuarioDeCuenta(_cuenta.CorreoElectronico);
+            }
+            catch (CommunicationException ex)
+            {
+                Console.WriteLine(ex);
+                resultado = Constantes.ERROR_CONEXION_SERVIDOR;
+                System.Windows.Forms.MessageBox.Show(Properties.Resources.msgErrorConexion);
+            }
+            catch (TimeoutException ex)
+            {
+                Console.WriteLine(ex);
+                resultado = Constantes.ERROR_CONEXION_SERVIDOR;
+                System.Windows.Forms.MessageBox.Show(Properties.Resources.msgErrorConexion);
+            }
+
+            switch (resultado)
+            {
+                case Constantes.OPERACION_EXITOSA:
+                    ColocarDatos();
+                    break;
+                case Constantes.OPERACION_EXITOSA_VACIA:
+                    break;
+                case Constantes.ERROR_CONEXION_BD:
+                    System.Windows.Forms.MessageBox.Show(Properties.Resources.msgErrorBaseDatos);
+                    break;
+                case Constantes.ERROR_CONSULTA:
+                    System.Windows.Forms.MessageBox.Show(Properties.Resources.msgErrorBaseDatos);
+                    break;
+                case Constantes.ERROR_CONEXION_SERVIDOR:
+                    System.Windows.Forms.MessageBox.Show(Properties.Resources.msgErrorConexion);
+                    break;
+            }
+        }
+
+        private void ClickSalir(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
