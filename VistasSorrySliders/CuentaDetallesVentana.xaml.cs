298bcf27492ab7a442e7a32d39adbf68608b3c91
melus7n7 <sulem477@gmail.com>
2023-10-20 08:33:23 -0600

    Ventana Detalles Cuenta

---
+++ VistasSorrySliders/CuentaDetallesVentana.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using VistasSorrySliders.ServicioSorrySliders;
+
+namespace VistasSorrySliders
+{
+    /// <summary>
+    /// Lógica de interacción para CuentaDetallesVentana.xaml
+    /// </summary>
+    public partial class CuentaDetallesVentana : Window
+    {
+        private CuentaSet _cuenta;
+        public CuentaDetallesVentana(CuentaSet cuenta)
+        {
+            InitializeComponent();
+            _cuenta = cuenta;
+            ColocarDatos();
+        }
+
+        private void ColocarDatos()
+        {
+            txtBoxCorreo.Text = _cuenta.CorreoElectronico;
+            txtBoxNickname.Text = _cuenta.Nickname;
+        }
+    }
+}
